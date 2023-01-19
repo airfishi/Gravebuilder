de64e3ba70834cfc597b5a917275631816608a3b
airfishi <fishthesurpeme@gmail.com>
2023-01-18 20:40:43 -0600

    movement and spawning for the large enemies is done

--- Grave Builder Project/Assets/Scripts/initial_move_dir.cs
+++ Grave Builder Project/Assets/Scripts/initial_move_dir.cs
@@ -8,39 +8,26 @@ public class initial_move_dir : MonoBehaviour
     public GameObject large_slime;
     Vector3 speed = Vector3.zero;
 
-    private bool movingright = false;
-    private bool movingleft = false;
-    private Vector3 left = new Vector3(-1,0,0);
-    private Vector3 right = new Vector3(1,0,0);
+    private float dirX;
     System.Random rand = new System.Random();
 
     void OnCollisionEnter2D(){
-        if(movingleft == false && movingright == false){
-            int changeTo = rand.Next(1);
-            if(changeTo == 0){
-                movingleft = true;
-                movingright = false;
-                speed = left;
-                large_slime.transform.localPosition+=speed;
-            }
-            else{
-                movingleft = false;
-                movingright = true;
-                speed = right;
-                large_slime.transform.localPosition+=speed;
-            }
+        if(dirX==0){
+            //randomly start the movement left or right when it lands
+            int changeTo = rand.Next(2);
+            if(changeTo == 1) dirX = -1;
+            else dirX = 1; 
         }
-        else if(movingleft == true && movingleft == false){
-            movingleft = false;
-            movingright = true;
-            speed = right;
-        }
-        else{
-            movingleft = true;
-            movingright = false;
-            speed = left;
-        }
+        else if(dirX == -1) dirX = 1; //swap the direction when slime collides
+        else dirX = -1;
+        speed = new Vector3(dirX,0,0);
     }
+
+    /*
+    void OnCollisionExit2D(){
+        large_slime.transform.localPosition+=speed;
+    }
+    */
 
     void Update(){
         large_slime.transform.localPosition+=speed;
--- Grave Builder Project/Assets/Scripts/large_slime_spawning.cs
+++ Grave Builder Project/Assets/Scripts/large_slime_spawning.cs
@@ -7,19 +7,24 @@ public class large_slime_spawning : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public float secondsBetweenSpawn = 20000;
+    public float secondsBetweenSpawn;
     public float elapsedTime = 0.0f;
     public GameObject large_slime;
+    private int xspawn;
+    private int posneg;
     System.Random rand = new System.Random();
 
     // Update is called once per frame
     void Update()
     {
         elapsedTime += Time.deltaTime;
-        //int posneg = rand.Next(1);
         if(elapsedTime > secondsBetweenSpawn){
             elapsedTime = 0;
-            Vector3 spawnPosition = new Vector3(rand.Next(100,200),8,0);
+            //randomly pick a place between -6 and 6 for slime to spawn
+            xspawn = rand.Next(3000);
+            posneg = rand.Next(2);
+            if(posneg == 1) xspawn = xspawn*-1;
+            Vector3 spawnPosition = new Vector3(xspawn,1500,0);
             GameObject newEnemy = (GameObject)Instantiate(large_slime,spawnPosition,Quaternion.Euler(0,0,0));
         }
     }
