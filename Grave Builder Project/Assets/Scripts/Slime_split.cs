3b1a9439d7e2547fbe08cf50b699fb5a59819af9
airfishi <fishthesurpeme@gmail.com>
2023-01-26 17:56:10 -0600

    everything works, just need sprites now

--- Grave Builder Project/Assets/Scripts/Slime_split.cs
+++ Grave Builder Project/Assets/Scripts/Slime_split.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadDetectScript : MonoBehaviour
+public class Slime_split : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject enemy;
+    //public GameObject enemy;
     public GameObject babyEnemy;
     private bool quitting = false;
     
     void Start()
     {
-        enemy = gameObject.transform.parent.gameObject;    
+     //   enemy = gameObject.transform.parent.gameObject;    
         quitting = false;
     }
 
@@ -22,16 +22,15 @@ public class HeadDetectScript : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        Destroy(gameObject);
-        Destroy(transform.parent.gameObject);
-        Destroy(transform.parent.transform.parent.gameObject);
+        if(collision.gameObject.tag.Equals("Player"))
+            Destroy(transform.parent.transform.parent.gameObject);
     }
 
     private void OnDestroy(){
         if(quitting == false){
             Vector3 right = new Vector3(250, 0, 0);
-            GameObject leftEnemy = (GameObject)Instantiate(babyEnemy,transform.parent.position - right,Quaternion.Euler(0,0,0));   
-            GameObject rightEnemy = (GameObject)Instantiate(babyEnemy,transform.parent.position + right,Quaternion.Euler(0,0,0));
+            GameObject leftEnemy = (GameObject)Instantiate(babyEnemy,transform.position - right,Quaternion.Euler(0,0,0));   
+            GameObject rightEnemy = (GameObject)Instantiate(babyEnemy,transform.position + right,Quaternion.Euler(0,0,0));
         }
     }
     
