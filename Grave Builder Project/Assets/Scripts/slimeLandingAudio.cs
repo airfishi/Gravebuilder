220b0e47362e40c8937c33a946909b38566741ec
airfishi <fishthesurpeme@gmail.com>
2023-02-09 17:39:03 -0600

    fixing slime walking/landing audio

--- Grave Builder Project/Assets/Scripts/slimeLandingAudio.cs
+++ Grave Builder Project/Assets/Scripts/slimeLandingAudio.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class slimeLandingAudio : MonoBehaviour
 {
 
-    public AudioClip saw;
+    public AudioClip land;
+    public AudioClip walk;
     bool landedOnce = false;
 
     void Start(){
         GetComponent<AudioSource>().playOnAwake = false;
-        GetComponent<AudioSource>().clip = saw;
-        
+        GetComponent<AudioSource>().clip = land;
     }
 
-    void Update() {
-        if(landedOnce){
-            
-        }
+    void Update(){
 
     }
 
-    void OnCollisionEnter2D(Collision2D collision){
-        if(!landedOnce){
+
+    //https://answers.unity.com/questions/1158528/wait-until-audio-is-finished-before-set-active-is.html
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!landedOnce)
+        {
             landedOnce = true;
+            GetComponent<AudioSource>().Play();
+            GetComponent<AudioSource>().clip = walk;
+            GetComponent<AudioSource>().loop = true;
             GetComponent<AudioSource>().Play();
         }
     }
