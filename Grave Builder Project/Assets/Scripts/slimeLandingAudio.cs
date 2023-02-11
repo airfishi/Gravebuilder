b530f2659c3ca29e923b4058ac0397ea4cd3f93e
airfishi <fishthesurpeme@gmail.com>
2023-02-11 13:35:58 -0600

    slime sounds for large slime work now, time for the medium slimes!

--- Grave Builder Project/Assets/Scripts/slimeLandingAudio.cs
+++ Grave Builder Project/Assets/Scripts/slimeLandingAudio.cs
@@ -7,30 +7,47 @@ public class slimeLandingAudio : MonoBehaviour
 
     public AudioClip land;
     public AudioClip walk;
-    bool landedOnce = false;
+    private bool landedOnce = false;
 
     void Start(){
         GetComponent<AudioSource>().playOnAwake = false;
-        GetComponent<AudioSource>().clip = land;
-    }
-
-    void Update(){
-
+        GetComponent<AudioSource>().loop = true;
     }
 
 
-    //https://answers.unity.com/questions/1158528/wait-until-audio-is-finished-before-set-active-is.html
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!landedOnce)
-        {
+        if(!landedOnce){
             landedOnce = true;
-            GetComponent<AudioSource>().Play();
-            GetComponent<AudioSource>().clip = walk;
-            GetComponent<AudioSource>().loop = true;
-            GetComponent<AudioSource>().Play();
+            StartCoroutine(playSlimeSound());
         }
     }
 
+    IEnumerator playSlimeSound(){
+        GetComponent<AudioSource>().clip = land;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
+        GetComponent<AudioSource>().clip = walk;
+        GetComponent<AudioSource>().Play();
+    }
 
 }
+/*
+//https://answers.unity.com/questions/904981/how-to-play-an-audio-file-after-another-finishes.html
+         public AudioClip engineStartClip;
+         public AudioClip engineLoopClip;
+         void Start()
+         {
+             GetComponent<AudioSource> ().loop = true;
+             StartCoroutine(playEngineSound());
+         }
+ 
+         IEnumerator playEngineSound()
+         {
+             audio.clip = engineStartClip;
+             audio.Play();
+             yield return new WaitForSeconds(audio.clip.length);
+             audio.clip = engineLoopClip;
+             audio.Play();
+         }
+*/
