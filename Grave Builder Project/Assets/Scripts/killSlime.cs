818695536e5dfa74406965d07a10d66791c4febc
airfishi <fishthesurpeme@gmail.com>
2023-02-16 20:48:55 -0600

    adjusted spawning positions and making the blocks children of the game screen

--- Grave Builder Project/Assets/Scripts/killSlime.cs
+++ Grave Builder Project/Assets/Scripts/killSlime.cs
@@ -6,7 +6,7 @@ public class killSlime : MonoBehaviour
 {
 
 public GameObject block;
-    public Transform gameScreen;
+
     private bool quitting = false;
     private Vector3 spawnloc = new Vector3(-5500,-1600,-2);
 
@@ -16,7 +16,7 @@ public GameObject block;
             //spawnloc.x = -5500 + 360*(xpos - 5500 % 360); 
             //~33 blocks across, 360 width change
             //28 blocks high, 240 height change
-            GameObject newEnemy = (GameObject)Instantiate(block,spawnloc,Quaternion.Euler(0,0,0), gameScreen);
+            GameObject newEnemy = (GameObject)Instantiate(block,spawnloc,Quaternion.Euler(0,0,0), transform.parent.transform.parent.gameObject.transform);
         }
     }
 
