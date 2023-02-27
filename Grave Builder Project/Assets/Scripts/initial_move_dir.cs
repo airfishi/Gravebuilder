4e3da54c44ba211ec7516936a425754fc6c0ba7f
airfishi <fishthesurpeme@gmail.com>
2023-02-27 12:46:52 -0600

    removed some commented old code from init_dir script ' :

--- Grave Builder Project/Assets/Scripts/initial_move_dir.cs
+++ Grave Builder Project/Assets/Scripts/initial_move_dir.cs
@@ -35,30 +35,8 @@ public class initial_move_dir : MonoBehaviour
         else if (GetComponent<Transform>().position.x < leftScreen) dirX = magnitude;
         else if (dirX == -magnitude) dirX = magnitude; //swap the direction when slime collides
         else dirX = -magnitude;
-        /*
-        if (dirX == 0)
-        {
-            if(animator)
-                animator.SetBool("isLanding", true);
-            //randomly start the movement left or right when it lands
-            int changeTo = rand.Next(2);
-            if (changeTo == 1) dirX = -magnitude;
-            else dirX = magnitude;
-        }
-        else if (GetComponent<Transform>().position.x > rightScreen) dirX = -magnitude;
-        else if (GetComponent<Transform>().position.x < leftScreen) dirX = magnitude;
-        else if (dirX == -magnitude) dirX = magnitude; //swap the direction when slime collides
-        else dirX = -magnitude;
-        speed = new Vector3(dirX,0,0);
-        */
     }
 
-    /*
-    void OnCollisionExit2D(){
-        large_slime.transform.localPosition+=speed;
-    }
-    */
-    
     void Update(){
         large_slime.transform.GetChild(0).GetComponent<Rigidbody2D>().velocity = new Vector2(dirX, large_slime.transform.GetChild(0).GetComponent<Rigidbody2D>().velocity.y);
     }    
