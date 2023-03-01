a2c04f00696faeba26291474f5265d23e3dba926
lmaschenberg <lmaschenberg@knox.edu>
2023-03-01 13:43:04 -0600

    Fixed block spawn height

--- Grave Builder Project/Assets/Scripts/blockSquashSlime.cs
+++ Grave Builder Project/Assets/Scripts/blockSquashSlime.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class blockSquashSlime : MonoBehaviour
 {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("LargeSlime") || collision.gameObject.tag.Equals("MediumSlime"))
         {
