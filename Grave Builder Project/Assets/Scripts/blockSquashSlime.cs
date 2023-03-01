ac7476161d0e04ecfd5ba10e7190ecb77e8627a4
lmaschenberg <lmaschenberg@knox.edu>
2023-03-01 13:45:29 -0600

    Started on large slime squash

--- Grave Builder Project/Assets/Scripts/blockSquashSlime.cs
+++ Grave Builder Project/Assets/Scripts/blockSquashSlime.cs
@@ -8,10 +8,23 @@ public class blockSquashSlime : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.tag.Equals("LargeSlime") || collision.gameObject.tag.Equals("MediumSlime"))
+        if (collision.gameObject.tag.Equals("MediumSlime"))
         {
 
             if (collision.transform.position.y+200 < GetComponent<Transform>().position.y)
+            {
+                Debug.Log("Destroyed!");
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+
+            }
+        }
+        else if (collision.gameObject.tag.Equals("LargeSlime"))
+        {
+
+            if (collision.transform.position.y -100 < GetComponent<Transform>().position.y)
             {
                 Debug.Log("Destroyed!");
                 Destroy(collision.gameObject);
