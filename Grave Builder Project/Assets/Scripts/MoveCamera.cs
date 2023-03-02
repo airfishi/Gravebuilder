836ecbe1403aac13e3d3f7bcddf918f89f63aa35
lmaschenberg <lmaschenberg@knox.edu>
2023-03-02 11:18:58 -0600

    The Camera MOVES!!!

--- Grave Builder Project/Assets/Scripts/MoveCamera.cs
+++ Grave Builder Project/Assets/Scripts/MoveCamera.cs
@@ -12,13 +12,14 @@ public class MoveCamera : MonoBehaviour
     private Vector3 moveBy = Vector3.zero;
     private int numBlocksInLevel = 1;
     private int blockSize = 250;
+    private int numBlocksInRow = 13;
 
     void Start()
     {
         for (int i = 0; i < 26; i++)    //Iterate through ys
         {
-            blocks[i] = new bool[26]; 
-            for (int j = 0; j < 26; j++)    //Iterate through xs
+            blocks[i] = new bool[numBlocksInRow]; 
+            for (int j = 0; j < numBlocksInRow; j++)    //Iterate through xs
             {
                 blocks[i][j] = false;
             }
@@ -36,7 +37,7 @@ public class MoveCamera : MonoBehaviour
     {
         for(int i = 25; i > 0; i--)
         {
-            for(int j = 0; j < 26; j++)
+            for(int j = 0; j < numBlocksInRow; j++)
             {
                 if (blocks[i][j] && !blocks[i - 1][j])
                 {
@@ -53,7 +54,7 @@ public class MoveCamera : MonoBehaviour
         for (int i = 0; i < 26; i++)//Iterate through y values
         {
             levels++;
-            for (int j = 0; j < 26; j++)    //Iterate through x values
+            for (int j = 0; j < numBlocksInRow; j++)    //Iterate through x values
             {
                 if (blocks[i][j] != true)
                 {
@@ -74,7 +75,7 @@ public class MoveCamera : MonoBehaviour
             well.transform.position += moveBy;
             for(int i = 0; i < numBlocksInLevel; i++)
             {
-                for(int j = 0; j < 26; j++)
+                for(int j = 0; j < numBlocksInRow; j++)
                 {
                     blocks[i][j] = false;
                 }
