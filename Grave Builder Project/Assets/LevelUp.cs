90d9a667d9c08fd22fafe8f5ea176e6d223666bd
lmaschenberg <lmaschenberg@knox.edu>
2023-03-02 15:10:07 -0600

    Began implementing LevelUp effects

--- Grave Builder Project/Assets/LevelUp.cs
+++ Grave Builder Project/Assets/LevelUp.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class LevelUp : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int time;
+    private GameObject well;
     void Start()
     {
-        
+        well = transform.parent.transform.GetChild(0).transform.GetChild(0).transform.GetChild(4).gameObject;
+        System.Random rnd = new System.Random();
+        int num = rnd.Next(0,5);
+        switch(num){
+            case 0:         //Accelerate Slime Spawning
+                well.GetComponent<large_slime_spawning>().secondsBetweenSpawn *= 2;
+                well.GetComponent<large_slime_spawning>().secondsBetweenSpawn /= 3;
+                break;
+            case 1:
+                well.GetComponent<large_slime_spawning>().clumpNumber++;
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        time++;
+        if(time > 1000)
+        {
+            Destroy(this);
+        }
     }
 }
